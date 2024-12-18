87436d16489060be7879099c366a54a44a0c3eef
IbrahimYasserM <2023170016@cis.asu.edu.eg>
2024-12-18 22:38:29 +0200

    fixing some errors

--- DataStructure/SegmentTree.cpp
+++ DataStructure/SegmentTree.cpp
@@ -1,15 +1,16 @@
+template <typename T>
 struct SegmentTree{
   #define L x*2+1
   #define R x*2+2
-  long long *a, *b;
+  T *a, *b;
   int n=1;
   explicit SegmentTree(int m){
     while(n<m) n*=2;
-    a = new long long[n*2]{};
-    b = new long long[n*2]{};
+    a = new T[n*2]{};
+    b = new T[n*2]{};
   }
-  long long merge(long long a, long long b){
-    long long c;
+  T merge(T a, T b){
+    T c;
     c = a + b;
     return c;
   }
@@ -18,7 +19,8 @@ struct SegmentTree{
     if(!~x) set(i,v,0,0,n);
     else if(rx-lx==1) a[x] = v;
     else {
-      b[L] = b[R] = b[x];
+      b[L] += b[x];
+      b[R] += b[x];
       b[x] = 0;
       int m = (lx+rx)/2;
       if(i<m) set(i,v,L,lx,m);
@@ -32,7 +34,8 @@ struct SegmentTree{
     else if(l>=rx||r<=lx);
     else if(r>=rx&&l<=lx) b[x] += v;
     else {
-      b[L] = b[R] = b[x];
+      b[L] += b[x];
+      b[R] += b[x];
       b[x] = 0;
       int m = (lx+rx)/2;
       add(l,r,v,L,lx,m);
@@ -41,25 +44,25 @@ struct SegmentTree{
     }
   }
   // get index
-  long long is(int i, int v, int x=-1, int lx=-1, int rx=-1){
-    if(!~x) return is(i,v,0,0,n);
-    else if(rx-lx==1) return a[x] + b[x]*(rx-lx);
-    else {
-      b[L] = b[R] = b[x];
-      b[x] = 0;
-      int m = (lx+rx)/2;
-      if(i<m) return is(i,v,L,lx,m);
-      else return is(i,v,R,m,rx);
-    }
-  }
-  // get range
-  long long get(int l, int r, int v, int x=-1, int lx=-1, int rx=-1){
-    if(!~x) return get(l,r,v,0,0,n);
-    if(l>=rx||r<=lx) return 0;
-    if(r>=rx&&l<=lx) return a[x] + b[x]*(rx-lx);
-    b[L] = b[R] = b[x];
+  T is(int i, int x=-1, int lx=-1, int rx=-1){
+    if(!~x) return is(i,0,0,n);
+    if(rx-lx==1) return a[x] + b[x];
+    b[L] += b[x];
+    b[R] += b[x];
     b[x] = 0;
     int m = (lx+rx)/2;
-    return merge(get(l,r,v,L,lx,m),get(l,r,v,R,m,rx));
+    if(i<m) return is(i,L,lx,m);
+    return is(i,R,m,rx);
+  }
+  // get range
+  T get(int l, int r, int x=-1, int lx=-1, int rx=-1){
+    if(!~x) return get(l,r,0,0,n);
+    if(l>=rx||r<=lx) return 0;
+    if(r>=rx&&l<=lx) return a[x] + b[x]*(rx-lx);
+    b[L] += b[x];
+    b[R] += b[x];
+    b[x] = 0;
+    int m = (lx+rx)/2;
+    return merge(get(l,r,L,lx,m),get(l,r,R,m,rx));
   }
 };
