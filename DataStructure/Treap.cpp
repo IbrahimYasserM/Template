156345dc4b400e39699c55938ac5b77ddacbd94b
IbrahimYasserM <153026269+IbrahimYasserM@users.noreply.github.com>
2026-06-21 17:53:41 +0300

    Implement Treap data structure with basic operations

---
+++ DataStructure/Treap.cpp
@@ -0,0 +1,65 @@
+typedef struct item* pitem;
+
+struct item {
+    int prior, cnt{1}, key;
+    pitem l{}, r{};
+
+    item(int key) : prior(rand()), key(key) {}
+};
+
+int cnt (pitem t) {
+    return t ? t->cnt : 0;
+}
+
+void upd (pitem t) {
+    if (t)
+        t->cnt = 1 + cnt(t->l) + cnt (t->r);
+}
+
+void split (pitem t, int key, pitem & l, pitem & r) {
+    if (!t)
+        l = r = nullptr;
+    else if (t->key <= key)
+        split (t->r, key, t->r, r),  l = t;
+    else
+        split (t->l, key, l, t->l),  r = t;
+    upd(t);
+}
+void insert (pitem & t, pitem it) {
+    if (!t)
+        t = it;
+    else if (it->prior > t->prior)
+        split (t, it->key, it->l, it->r),  t = it;
+    else
+        insert (t->key <= it->key ? t->r : t->l, it);
+}
+
+void merge (pitem & t, pitem l, pitem r) {
+    if (!l || !r)
+        t = l ? l : r;
+    else if (l->prior > r->prior)
+        merge (l->r, l->r, r),  t = l;
+    else
+        merge (r->l, l, r->l),  t = r;
+    upd(t);
+}
+
+void erase (pitem & t, int key) {
+    if (t->key == key) {
+        pitem th = t;
+        merge (t, t->l, t->r);
+        delete th;
+    }
+    else
+        erase (key < t->key ? t->l : t->r, key);
+}
+
+pitem unite (pitem l, pitem r) {
+    if (!l || !r)  return l ? l : r;
+    if (l->prior < r->prior)  std::swap (l, r);
+    pitem lt, rt;
+    split (r, l->key, lt, rt);
+    l->l = unite (l->l, lt);
+    l->r = unite (l->r, rt);
+    return l;
+}
